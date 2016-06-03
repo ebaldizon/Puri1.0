03e6cb3379884373283aa874c5a9ae1c3db3d770
ebaldizon <emanuel.baldizon@gmail.com>
2016-06-02 19:40:11 -0600

    LinkedList - Remove Obj

--- Data/Lists/LinkedList.php
+++ Data/Lists/LinkedList.php
@@ -4,18 +4,23 @@
   {
     private $head;
     private $tail;
+    private $longest;
 
-    public function __construct(){}
+    public function __construct()
+    {
+      $longest = 0;
+    }
 
 /********************************/
-    public function add_obj($data)
+    public function add_obj($obj)
     {
 
-      $newNode = new Node($data);
+      $newNode = new Node($obj);
 
       if($this->head == NULL)
       {
         $this->head = $newNode;
+        $longest++;
       }
       else
       {
@@ -23,16 +28,67 @@
         {
           $this->head->setNext($newNode);
           $this->tail = $newNode;
+          $longest++;
         }
         else
         {
           $this->tail->setNext($newNode);
           $this->tail = $newNode;
+          $longest++;
         }
       }
     }
-  /******************************/
-    public function viewContent()
+
+    /*****************************  without variable $found
+    public function remove_obj_1($obj)
+    {
+      $wanted = new Node();
+      $wanted->setData($obj);
+      $runner = $this->head;
+
+      while($runner->getData() != $wanted->getData())
+      {
+        if($runner->getNext()->getData() == $wanted->getData())
+        {
+          $bodyRest = $runner->getNext()->getNext();
+          $runner->setNext($bodyRest);
+          $this->longest--;
+        }
+        else
+        {
+          echo $runner->getData();
+          $runner = $runner->getNext();
+        }
+      }
+    }
+    *************************************/
+
+    public function remove_obj($obj)
+    {
+      $wanted = new Node();
+      $wanted->setData($obj);
+      $runner = $this->head;
+      $found = false;
+
+      while($found != true)
+      {
+        if($runner->getNext()->getData() == $wanted->getData())
+        {
+          $bodyRest = $runner->getNext()->getNext();
+          $runner->setNext($bodyRest);
+          $this->longest--;
+          return $found = true;
+        }
+        else
+        {
+          echo $runner->getData();
+          $runner = $runner->getNext();
+        }
+      }
+      return false;
+    }
+
+    public function viewAllContent()
     {
       $runner = $this->head;
 
