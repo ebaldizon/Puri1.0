cad1a0b14a9556fce035a22bbc0bca94cac3ac73
ebaldizon <emanuel.baldizon@gmail.com>
2016-06-07 15:14:10 -0600

    LinkedList-Generic

--- Data/Lists/LinkedList.php
+++ Data/Lists/LinkedList.php
@@ -4,22 +4,30 @@
   {
     private $head;
     private $tail;
-    private $longest;
+    private $longest = 0;
 
     public function __construct()
     {
-      $longest = 0;
+    }
+
+    public function getHead()
+    {
+      return $this->head;
+    }
+
+    public function getTail()
+    {
+      return $this->tail;
     }
 
     public function add_obj($obj)
     {
-
       $newNode = new Node($obj);
 
       if($this->head == NULL)
       {
         $this->head = $newNode;
-        $longest++;
+        $this->longest++;
       }
       else
       {
@@ -27,83 +35,114 @@
         {
           $this->head->setNext($newNode);
           $this->tail = $newNode;
-          $longest++;
+          $this->longest++;
         }
         else
         {
           $this->tail->setNext($newNode);
           $this->tail = $newNode;
-          $longest++;
+          $this->longest++;
         }
       }
     }
 
+    /*Busca y retorna el nodo del objeto buscado*/
     public function find_obj_1($obj)
     {
-      $wanted = new Node();
-      $wanted->setData($obj);
+      $wanted = new Node($obj);
       $runner = $this->head;
       $found = false;
 
-      for($i=0;$i<=$longest;$i++)
-      if($runner->getData() == $wanted->getData())
-      {
-        return $runner;
-      }
-      return false;
-    }
-
-    public function find_obj_2($obj)
-    {
-      $wanted = new Node();
-      $wanted->setData($obj);
-      $runner = $this->head;
-      $found = false;
-
-      for($i=0;$i<=$longest;$i++)
-      if($runner->getNext()->getData() == $wanted->getData())
-      {
-        return $runner;
-      }
-      return false;
-    }
-
-    public function remove_obj_1($obj)
-    {
-      $wanted = new Node();
-      $wanted->setData($obj);
-      $runner = $this->head;
-      $found = false;
-
-      while($found != true)
-      {
-        if($runner->getNext()->getData() == $wanted->getData())
+      for($i=0;$i<=$this->longest;$i++){
+        if($runner->getData() == $wanted->getData())
         {
-          $bodyRest = $runner->getNext()->getNext();
-          $runner->setNext($bodyRest);
-          $this->longest--;
-          return $found = true;
+          return $runner;
         }
         else
         {
-          echo $runner->getData();
           $runner = $runner->getNext();
         }
       }
       return false;
     }
 
-    public function remove_obj_2($obj)
+    /*Busca y retornar el nodo siguiente del objeto buscado*/
+    public function find_obj_2($obj)
     {
-      $beforeNode = find_obj_2($obj);
-      if($beforeNode != false)
+      $wanted = new Node($obj);
+      $runner = $this->head;
+      $found = false;
+
+      for($i=0;$i<=$this->longest;$i++)
+      if($runner->getNext()->getData() == $wanted->getData())
       {
-        $beforeNode->setNext($beforeNode->getNext()->getNex());
-        return true;
+        return $runner;
+      }
+      else
+      {
+        $runner = $runner->getNext();
       }
       return false;
     }
 
+    public function mod_obj_1($obj1, $obj2)
+    {
+      if($this->longest != 0)
+      {
+          $wanted = $this->find_obj_1($obj1);
+
+        if($wanted != false)
+        {
+          $wanted->setData($obj2);
+          return true;
+        }
+        else
+        {
+            return false;
+        }
+      }
+      else
+      {
+        return false;
+      }
+    }
+
+    public function remove_obj_1($obj)
+    {
+      if($this->longest != 0)
+      {
+        $wanted = new Node($obj);
+
+        if($this->longest == 1 && $this->head->getData() == $wanted->getData())
+        {
+          $this->head == NULL;
+          return true;
+        }
+        else if ($this->longest > 1 && $this->head->getData() == $wanted->getData())
+        {
+          $this->head = $this->head->getNext();
+          return true;
+        }
+        else
+        {
+          $before = $this->find_obj_2($obj);
+          if($before->getNext()->getData() == $this->tail->getData())
+          {
+            $before->setNext(NULL);
+            $this->tail = $before;
+          }
+          else
+          {
+            $before = $before->setNext($before->getNext()->getNext());
+          }
+          return true;
+        }
+      }
+      else
+      {
+        return false;
+      }
+    }
 
     public function viewAllContent()
     {
@@ -115,7 +154,6 @@
         $runner = $runner->getNext();
       }
     }
-
     //End class
   }
 
--- Data/Lists/Test.php
+++ Data/Lists/Test.php
@@ -10,6 +10,17 @@
   $list->add_obj(" Atom!");
   $list->add_obj(", Bye...");
 
-  $list->viewContent();
+  $list->viewAllContent();
 
+  echo "<br/>Find.".$list->find_obj_1(" mundo,")->getData();
+  echo "<br/>Find.".$list->find_obj_2(" mundo,")->getData();
+  echo "<br/>";
+  echo "culita".$list->getTail()->getData();
+  echo "<br/>";
+  echo "<br/>";
+  echo $list->remove_obj_1(", Bye...");
+  echo "<br/>";
+  echo "<br/>";
+  //$list->mod_obj_1(" Atom!", "aToMiC?");
+  $list->viewAllContent();
 ?>
