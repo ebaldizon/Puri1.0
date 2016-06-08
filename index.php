5108144679b6ea1ffa99e44b812b3278f63f0b65
ebaldizon <emanuel.baldizon@gmail.com>
2016-06-08 15:59:46 -0600

    Frontend-Index

--- index.php
+++ index.php
@@ -3,9 +3,46 @@
   <head>
     <meta charset="utf-8" name="viewport" content="initial-scale=1, maximum-scale=1">
     <link rel="stylesheet" href="Presentation/Bootstrap/css/bootstrap.css" media="screen">
+    <link href="Presentation/Bootstrap/assets/css/bootstrap.css" rel="stylesheet">
+    <link href="Presentation/Bootstrap/assets/css/font-awesome.css" rel="stylesheet">
+    <link href="Presentation/Bootstrap/assets/css/docs.css" rel="stylesheet" >
+    <link href="Presentation/Bootstrap/bootstrap-social.css" rel="stylesheet" >
+
+    <link rel="stylesheet" href="Presentation/CSS/styles.css">
+    <link href='https://fonts.googleapis.com/css?family=Homenaje' rel='stylesheet' type='text/css'>
+    <title>Purificadores de agua - Costa Rica</title>
   </head>
   <body>
+    <div class="container">
+      <div class="row">
+        <div class="col-md-5">
+          <br/>
+          <p class="p-banner-1">CORPORACIÓN BALRY J Y E S.A.</p>
+          <p class="p-banner-2">PURIFICADORES DE AGUA - COSTA RICA</p>
+          <br/>
+        </div>
+        <div class="col-md-4">
 
+        </div>
+        <div class="col-md-3">
+          <br>
+          <br>
+          <a class="btn btn-social-icon btn-facebook">
+            <span class="fa fa-facebook"></span>
+          </a>
+          <a class="btn btn-social-icon btn-twitter">
+            <span class="fa fa-twitter"></span>
+          </a>
+          <a class="btn btn-social-icon btn-google">
+            <span class="fa fa-google"></span>
+          </a>
+          <a class="btn btn-social-icon btn-pinterest">
+            <span class="fa fa-pinterest"></span>
+          </a>
+        </div>
+      </div>
+
+    </div>
     <nav class="navbar navbar-default">
       <div class="container-fluid">
         <!-- Brand and toggle get grouped for better mobile display -->
@@ -13,68 +50,147 @@
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
               <span class="sr-only">Toggle navigation</span>
             </button>
-            <a class="navbar-brand" href="#"><img src=""/></a>
           </div>
+          <div class="container">
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Inicio <span class="sr-only">(current)</span></a></li>
+                <li><a href="Presentation/Nosotros.php">Nosotros</a></li>
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Purificadores<span class="caret"></span></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="Presentation/Purifiers/Essential.php">Essential</a></li>
+                    <li><a href="Presentation/Purifiers/OsmosisInversa.php">&Oacute;mosis Inversa</a></li>
+                    <li><a href="Presentation/Purifiers/Ozono.php">Ozono</a></li>
+                    <li><a href="Presentation/Purifiers/Ultravioleta.php">Ultravioleta</a></li>
+                    <li role="separator" class="divider"></li>
+                    <li><a href="Presentation/Purifiers/Dispensador.php">Dispensadores</a></li>
+                    <li role="separator" class="divider"></li>
+                    <li><a href="Presentation/Purifiers/Industrial.php">Industriales</a></li>
+                  </ul>
+                </li>
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Cocina<span class="caret"></span></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="Presentation/Kitchen/Sarten.php">Sartenes</a></li>
+                    <li><a href="Presentation/Kitchen/Cacerola.php">Cacerolas</a></li>
+                    <li><a href="#">Asadores el&eacute;ctricos</a></li>
+                    <li role="separator" class="divider"></li>
+                    <li><a href="Presentation/Kitchen/SetCubiertos.php">Set de cubiertos</a></li>
+                    <li><a href="Presentation/Kitchen/SetOllas.php">Set de ollas</a></li>
+                  </ul>
+                </li>
+                <li><a href="#">Servicios</a></li>
+                <li><a href="Presentation/Contactenos.php">Cont&aacute;ctenos</a></li>
+              </ul>
+              <form class="navbar-form navbar-left" role="search">
+                <div class="form-group">
+                  <input type="text" class="form-control" placeholder="">
+                </div>
+                <button type="submit" class="btn btn-default">Buscar</button>
+              </form>
 
+              <ul class="nav navbar-nav navbar-right">
+                <!--<li><a href="#">Link3</a></li>-->
+                <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Mi cuenta<span class="caret"></span></a>
+                  <ul class="dropdown-menu">
+                    <li><a href="#">Ingresar</a></li>
+                    <li role="separator" class="divider"></li>
+                    <li><a href="#">Registrarse</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </div><!-- /.navbar-collapse -->
+          </div>
           <!-- Collect the nav links, forms, and other content for toggling -->
-          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-            <ul class="nav navbar-nav">
-              <!--<li class="active"><a href="#">Inicio <span class="sr-only">(current)</span></a></li>-->
-              <li><a href="#">Nosotros</a></li>
-              <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Purificadores<span class="caret"></span></a>
-                <ul class="dropdown-menu">
-                  <li><a href="#">Essential</a></li>
-                  <li><a href="#">&Oacute;mosis Inversa</a></li>
-                  <li><a href="#">Ozono</a></li>
-                  <li><a href="#">Ultravioleta</a></li>
-                  <li role="separator" class="divider"></li>
-                  <li><a href="#">Dispensadores</a></li>
-                  <li role="separator" class="divider"></li>
-                  <li><a href="#">Industriales</a></li>
-                </ul>
-              </li>
-              <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Cocina<span class="caret"></span></a>
-                <ul class="dropdown-menu">
-                  <li><a href="#">Sartenes</a></li>
-                  <li><a href="#">Cacerolas</a></li>
-                  <li><a href="#">Asadores el&eacute;ctricos</a></li>
-                  <li role="separator" class="divider"></li>
-                  <li><a href="#">Set de cubiertos</a></li>
-                  <li><a href="#">Set de ollas</a></li>
-                </ul>
-              </li>
-            </ul>
-            <form class="navbar-form navbar-left" role="search">
-              <div class="form-group">
-                <input type="text" class="form-control" placeholder="">
-              </div>
-              <button type="submit" class="btn btn-default">Buscar</button>
-            </form>
-
-            <ul class="nav navbar-nav navbar-right">
-              <!--<li><a href="#">Link3</a></li>-->
-              <li><a href="#">Cont&aacute;ctenos</a></li>
-              <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Mi cuenta<span class="caret"></span></a>
-                <ul class="dropdown-menu">
-                  <li><a href="#">Action</a></li>
-                  <li><a href="#">Another action</a></li>
-                  <li><a href="#">Something else here</a></li>
-                  <li role="separator" class="divider"></li>
-                  <li><a href="#">Separated link</a></li>
-                </ul>
-              </li>
-            </ul>
-          </div><!-- /.navbar-collapse -->
       </div><!-- /.container-fluid -->
     </nav>
-
-
+    <div class="container main">
+      <div id="carousel-example-generic" class="carousel slide slider-banner" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+          <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+          <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+          <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+        </ol>
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner" role="listbox">
+          <div class="item active">
+            <img src="Presentation/img/slider-1.jpg" alt="...">
+            <div class="carousel-caption">
+              <h4>Purificadores de agua</h4>
+            </div>
+          </div>
+          <div class="item">
+            <img src="Presentation/img/slider-2.jpg" alt="...">
+            <div class="carousel-caption">
+              <h4>Accesorios de cocina</h4>
+            </div>
+          </div>
+          <div class="item">
+            <img src="Presentation/img/slider-3.jpg" alt="...">
+            <div class="carousel-caption">
+              <h4>Purificadores de aire</h4>
+            </div>
+          </div>
+        </div>
+        <!-- Controls -->
+        <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+          <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+          <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+          <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+          <span class="sr-only">Next</span>
+        </a>
+      </div><!--END SLIDER -->
+      <br>
+      <div class="row">
+        <div class="col-sm-6 col-md-4">
+          <div class="thumbnail">
+            <img class="img-thumbnails" src="Presentation/img/logo_balry.png" alt="...">
+            <div class="caption">
+              <h3>Thumbnail label</h3>
+              <p>asdnaiusd asd nasui asdun adsuin dsan adsuinsad ansd ansdui ansd
+                asdnaiusd asd nasui asdun adsuin dsan adsuinsad ansd ansdui ansd
+              </p>
+              <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4">
+          <div class="thumbnail">
+            <img class="img-thumbnails" src="Presentation/img/logo_balry.png" alt="...">
+            <div class="caption">
+              <h3>Thumbnail label</h3>
+              <p>asdnaiusd asd nasui asdun adsuin dsan adsuinsad ansd ansdui ansd
+                asdnaiusd asd nasui asdun adsuin dsan adsuinsad ansd ansdui ansd
+              </p>
+              <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6 col-md-4">
+          <div class="thumbnail">
+            <img class="img-thumbnails" src="Presentation/img/logo_balry.png" alt="...">
+            <div class="caption">
+              <h3>Thumbnail label</h3>
+              <p>asdnaiusd asd nasui asdun adsuin dsan adsuinsad ansd ansdui ansd
+                asdnaiusd asd nasui asdun adsuin dsan adsuinsad ansd ansdui ansd
+              </p>
+              <p><a href="#" class="btn btn-primary" role="button">Button</a> <a href="#" class="btn btn-default" role="button">Button</a></p>
+            </div>
+          </div>
+        </div>
+      </div><!--ROW-->
+    </div>
+    <footer class="footer">
+      <div class="container">
+        <p class="text-muted">Web Latina ©</p>
+      </div>
+    </footer>
     <script src="http://code.jquery.com/jquery-latest.js"></script>
     <script src="Presentation/Bootstrap/js/bootstrap.js"></script>
   </body>
-
-
 </html>
